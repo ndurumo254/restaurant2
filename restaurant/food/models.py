3282f53c0fcc8f3990b46e2223bec0859fb22dba
unknown <erickndurumo@gmail.com>
2022-07-05 08:21:13 -0700

    added slug in the models

--- restaurant/food/models.py
+++ restaurant/food/models.py
@@ -1,4 +1,6 @@
+from contextlib import suppress
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Item(models.Model):
@@ -8,6 +10,13 @@ class Item(models.Model):
     price= models.DecimalField(max_digits=10, decimal_places=2)
     time_to_prepare= models.IntegerField()
     food_image=models.ImageField (upload_to='Item/')
+    slug= models.SlugField(blank=True,null= True)
+
+
+def save(self, *args,**kwargs):
+    if not self.slug and self.name:
+        self.slugify(self.name)
+    super(Item, self).save(**args , **kwargs)
 
 
 
