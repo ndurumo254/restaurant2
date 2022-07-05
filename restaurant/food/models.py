a093ae6c8666179b09926f09ede2cbfb20aca13e
unknown <erickndurumo@gmail.com>
2022-07-04 19:53:45 -0700

    created models in my food app

--- restaurant/food/models.py
+++ restaurant/food/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name=models.CharField(max_length=200)
+    desc=models.TextField(max_length=100)
+    people= models.IntegerField(blank=True, null=True)
+    price= models.DecimalField(max_digits=10, decimal_places=2)
+    time_to_prepare= models.IntegerField()
+    food_image=models.ImageField (upload_to='food/')
